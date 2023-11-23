4889b2ea95ef44f750321402bef3b8808d136707
WillianJMeurer <142546243+WillianJMeurer@users.noreply.github.com>
2023-11-23 19:21:24 -0300

    Update Veiculo.java

---
+++ Prova_POO/src/main/Veiculo.java
@@ -0,0 +1,27 @@
+package main;
+
+public class Veiculo {
+    private int anoFabricacao;
+    private double carga;
+
+    public Veiculo(int anoFabricacao, double carga) {
+        this.anoFabricacao = anoFabricacao;
+        this.carga = carga;
+    }
+
+    public int getAnoFabricacao() {
+        return anoFabricacao;
+    }
+
+    public void setAnoFabricacao(int anoFabricacao) {
+        this.anoFabricacao = anoFabricacao;
+    }
+
+    public double getCarga() {
+        return carga;
+    }
+
+    public void setCarga(double carga) {
+        this.carga = carga;
+    }
+} 
